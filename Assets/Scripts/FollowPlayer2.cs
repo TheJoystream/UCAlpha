6b309f53d576855dc5da85acacb3e1fc712aa7a5
TheJoystream <Dean.millen88@gmail.com>
2024-10-24 12:16:28 +0100

    Camera Set

--- Assets/Scripts/FollowPlayer2.cs
+++ Assets/Scripts/FollowPlayer2.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FollowPlayer2 : MonoBehaviour
 {
     public GameObject player;
-    private Vector3 offset = new Vector3(0, 90, -80);
+    private Vector3 offset = new Vector3(0, 90, -40);
 
     // Start is called before the first frame update
     void Start()
@@ -14,7 +14,7 @@ public class FollowPlayer2 : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         transform.position = player.transform.position + offset;
     }
