ee6a651526a4d45a3f73a28064ee2f4596bf1727
TheJoystream <Dean.millen88@gmail.com>
2024-10-25 11:55:35 +0100

    Basic UI, items are picked up, gameObjects destroyed

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,14 @@
+[System.Serializable]
+
+public class Item
+
+{
+    public string name;
+    public int count;
+
+    public Item(string itemName, int itemCount)
+    {
+        name = itemName;
+        count = itemCount;
+    }
+}
