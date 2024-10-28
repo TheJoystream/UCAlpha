7b3fdc9539ee9123f5a653039196f17a1168d203
TheJoystream <Dean.millen88@gmail.com>
2024-10-28 22:15:18 +0000

    AI working better

--- Assets/Scripts/Enemy/EnemyFollow.cs
+++ Assets/Scripts/Enemy/EnemyFollow.cs
@@ -1,16 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class EnemyFollow : MonoBehaviour
 {
     public NavMeshAgent enemy;
     public Transform Player;
+    public float radius;
+    public Transform[] patrolPoints;
+    public int targetPoint;
+    public float speed;
+   
+
+    
 
     // Start is called before the first frame update
     void Start()
     {
+        targetPoint = 0;
         
     }
 
@@ -18,5 +28,27 @@ public class EnemyFollow : MonoBehaviour
     void Update()
     {
         enemy.SetDestination(Player.position);
+    
+    
+       if (transform.position == patrolPoints[targetPoint].position)
+        {
+            increaseTargetInt();
+
+}
+transform.position = Vector3.MoveTowards(transform.position, patrolPoints[targetPoint].position, speed * Time.deltaTime);
+
+        
+transform.LookAt(patrolPoints[targetPoint].position);
+
+        
     }
+    
+
+    void increaseTargetInt()
+{
+    targetPoint++;
+    if (targetPoint >= patrolPoints.Length)
+    { targetPoint = 0; }
+}
+    
 }
